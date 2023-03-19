5d7867d1976caeb515f9c52692139b2a1057e54e
Natworpong Loyswai <28857628+rsxss@users.noreply.github.com>
2023-03-19 10:20:22 +0700

    Update Insert.sql

--- Insert.sql
+++ Insert.sql
@@ -1,28 +1,40 @@
--- select host, user from mysql.user;
+```
+CREATE DATABASE assignment_project;
+# User SQL Query
 
--- create database assignment_project;
--- -- drop database assignment_project;
-use assignment_project;
-select * from Homeworks_stu_homework;
-show tables;  
+INSERT INTO `homeworks_teacher`(teacher_id, fname, lname, birthday, email, phone, password) VALUES(1,'Admin', 'Admin', '1995-02-22', 'Admin', '0904751326', 'Admin');
 
--- DELETE FROM Homeworks_homework WHERE homework_id = 3;
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(1,'คณิตศาสตร์ ', '', 3, 1);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(2,'วิทยาศาสตร์ ', '', 3, 2);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(3,'อังกฤษ', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(4,'ไทย', '', 3, 4);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(5,'พละ', '', 3, 1);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(6,'สุขศึกษา', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(7,'ดณตรี', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(8,'เปียโน', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(9,'Science', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(10,'Mathematic', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(11,'ENS', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(12,'สังคมศึกษา ศาษนาและวัฒนธรรม', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(13,'ประวัติศาครื', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(14,'Social Study', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(15,'จีน', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(16,'Computer', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(17,'Stem', '', 3, 3);
+INSERT INTO `homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(18,'แนะแนว', '', 3, 3);
 
-INSERT INTO `Homeworks_teacher`(teacher_id, fname, lname, birthday, email, phone, password) VALUES(1,'Sarah', 'Moore', '1990-02-20', 'sarah.moor@gmail.com', '0912356478', '');
-INSERT INTO `Homeworks_teacher`(teacher_id, fname, lname, birthday, email, phone, password) VALUES(2,'Emily', 'Smith', '1995-02-22', 'emily.smit@gmail.com', '0904751326', '');
+INSERT INTO `homeworks_grade`(grade_id,name, num) VALUES(1,'ประถมศึกษา', 1);
+INSERT INTO `homeworks_grade`(grade_id,name, num) VALUES(2,'ประถมศึกษา', 2);
+INSERT INTO `homeworks_grade`(grade_id,name, num) VALUES(3,'ประถมศึกษา', 3);
+INSERT INTO `homeworks_grade`(grade_id,name, num) VALUES(4,'ประถมศึกษา', 4);
+INSERT INTO `homeworks_grade`(grade_id,name, num) VALUES(5,'ประถมศึกษา', 5);
+INSERT INTO `homeworks_grade`(grade_id,name, num) VALUES(6,'ประถมศึกษา', 6);
 
-INSERT INTO `Homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(1,'คณิตศาสตร์ 1', '', 3, 1);
-INSERT INTO `Homeworks_subject`(subject_id,name, description, credit, teacher_id) VALUES(2,'วิทยาศาสตร์ 1', '', 3, 2);
+INSERT INTO `homeworks_room`(room_id,num, grade_id, subject_id) VALUES(1,1, 4, 1);
+INSERT INTO `homeworks_room`(room_id,num, grade_id, subject_id) VALUES(2,1, 4, 2);
 
-INSERT INTO `Homeworks_grade`(grade_id,name, num) VALUES(1,'ประถมศึกษา', 1);
-INSERT INTO `Homeworks_grade`(grade_id,name, num) VALUES(2,'ประถมศึกษา', 2);
-INSERT INTO `Homeworks_grade`(grade_id,name, num) VALUES(3,'ประถมศึกษา', 3);
-INSERT INTO `Homeworks_grade`(grade_id,name, num) VALUES(4,'ประถมศึกษา', 4);
-INSERT INTO `Homeworks_grade`(grade_id,name, num) VALUES(5,'ประถมศึกษา', 5);
-INSERT INTO `Homeworks_grade`(grade_id,name, num) VALUES(6,'ประถมศึกษา', 6);
+INSERT INTO `homeworks_student`(student_id,fname, lname, phone, birthday, email, room) VALUES(3,'Adminpage', 'str01', '0823416383', '2013-03-13', 'Adminpagestr01', 1);
+INSERT INTO `homeworks_student`(student_id,fname, lname, phone, birthday, email, room) VALUES(4,'Adminpage', 'str02', '0823416383', '2013-03-13', 'Adminpagestr02', 2);
 
-INSERT INTO `Homeworks_room`(room_id,num, grade_id, subject_id) VALUES(1,1, 4, 1);
-INSERT INTO `Homeworks_room`(room_id,num, grade_id, subject_id) VALUES(2,1, 4, 2);
-
-INSERT INTO `Homeworks_student`(student_id,fname, lname, birthday, email, phone, room) VALUES(1,'Bob', 'Migon', '2013-07-30', 'bob.migo@gmail.com', '0826547891', 1);
-INSERT INTO `Homeworks_student`(student_id,fname, lname, birthday, email, phone, room) VALUES(2,'Mae', 'Pooka', '2013-09-17', 'mae.pook@gmail.com', '0824561372', 1);
+select * from homeworks_teacher
+```
